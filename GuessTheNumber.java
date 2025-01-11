21805f3c3c5fc65543267dd61cec818158aee56c
Amulya M S <amulyyaaa@gmail.com>
2025-01-11 16:35:40 +0530

    Add files via upload

---
+++ GuessTheNumber.java
@@ -0,0 +1,52 @@
+// Java program for the above approach
+import java.util.Scanner;
+
+public class GuessTheNumber 
+{
+  public static void main(String arg[])
+	{
+     guessingNumberGame();// Driver Code
+	}
+
+	
+	public static void guessingNumberGame() // Function that implements the number guessing game
+	{
+		
+		Scanner sc= new Scanner(System.in);
+		int number = 1 + (int)(100* Math.random());
+		int K = 5;
+    int i, guess;
+    System.out.println("A number is chosen" + " between 1 to 100."+ "Guess the number"+ " within 5 trials.");
+		for (i = 0; i < K; i++) 
+    {
+
+			System.out.println("Guess the number:");
+      guess = sc.nextInt();
+      if (number == guess) 
+      {
+				System.out.println("Congratulations!"+ " You guessed the number.");
+        break;
+			}
+			else if (number > guess && i != K - 1) 
+      {
+				System.out.println("The number is "+ "greater than " + guess);
+			}
+			else if (number < guess && i != K - 1) 
+      {
+				System.out.println("The number is" + " less than " + guess);
+			}
+		}
+    
+
+		if (i == K) 
+    {
+			System.out.println("You have exhausted"+ " 5 trials.");
+			System.out.println("The number was " + number);				
+		}
+    sc.close();
+	}
+
+
+	
+	
+}
